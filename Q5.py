d6acba10d9c846feb433edb60e46f44c993d0dea
ikrishnamishraa <krishna.1212mishra@gmail.com>
2025-04-29 12:16:16 +0530

    Add files via upload

---
+++ Q5.py
@@ -0,0 +1,13 @@
+print("Name: Krishna Mishra ")
+print("Roll No: 24BEE109")
+list =[]
+def upper(l):
+    l =["hello","bye","Python","how","hey"]
+    g=[]
+    for i in range(len(l)):
+        f=l[i].upper()
+        g.append(f)
+    print(g)
+       
+upper(list)
+        
