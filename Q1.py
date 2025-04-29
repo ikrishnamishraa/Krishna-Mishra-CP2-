f5dd73a00847f62f603651c14a330726361c5c6e
ikrishnamishraa <krishna.1212mishra@gmail.com>
2025-04-29 12:26:25 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,17 @@
+print("Name: Krishna Mishra ")
+print("Roll No: 24BEE109")
+def fun():
+    print("This is function fun()")
+
+def disp():
+    print("This is function disp()")
+
+def msg():
+    print("This is function msg()")
+
+
+functions = [fun, disp, msg]
+
+
+for func in functions:
+    func()
