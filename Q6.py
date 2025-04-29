aafa3d05c240c858fe713bacad0069ca944019a7
ikrishnamishraa <krishna.1212mishra@gmail.com>
2025-04-29 12:00:43 +0530

    Add files via upload

---
+++ Q6.py
@@ -0,0 +1,15 @@
+print("Name: Krishna Mishra ")
+print("Roll No: 24BEE109")
+def print_day_with_suffixes():
+    for hour in range(24):  
+        if hour == 0:
+            print(f"12:00 AM - Midnight")
+        elif hour == 12:
+            print(f"12:00 PM - Noon")
+        elif hour < 12:
+            print(f"{hour}:00 AM")
+        else:
+            print(f"{hour - 12}:00 PM")
+
+
+print_day_with_suffixes()
